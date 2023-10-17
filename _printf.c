d9b890fffc38c413334583f58ccdfdc8a904232e
Globski <gloria.ogunsemore@gmail.com>
2023-10-17 06:40:46 -0700

    _printf.c

---
+++ _printf.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ * _printf - Custom printf function that handles %c, %s, and %%.
+ * @format: The format string to be printed.
+ *
+ * Return: The number of characters printed.
+ */

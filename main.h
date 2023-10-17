63f40d4b2645bafe0065ceb586a841f49df55694
Globski <gloria.ogunsemore@gmail.com>
2023-10-17 06:28:37 -0700

    Add prototype to main.h

--- main.h
+++ main.h
@@ -5,6 +5,10 @@
 * Description: Header file containing prototypes for all functions
 * used in the printf project.
 */
+
+#include <stddef.h>
+#include <stdarg.h>
+
 int _putchar(char c);
 
 #endif /* #ifndef MAIN_H */

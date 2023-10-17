c93ee373f1ebe3cf642d475880792f6c5f1e99e8
Globski <gloria.ogunsemore@gmail.com>
2023-10-17 07:07:26 -0700

    implementation of the _printf project

--- _printf.c
+++ _printf.c
@@ -5,3 +5,20 @@
  *
  * Return: The number of characters printed.
  */
+
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0, i;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;

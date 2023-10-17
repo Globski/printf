0f0d4e4757d76791e04633064f9c631d36b50964
Manuelshub <kcemmy03@gmail.com>
2023-10-17 06:59:13 -0700

    implemented a conditioinal statement

--- handle_specifier.c
+++ handle_specifier.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * handle_specifier - Handle format specifiers in the printf function.
  * @format: The format string being processed.
@@ -6,3 +7,9 @@
  * @count: A pointer to the character count.
  * @i: A pointer to the current position in the format string.
  */
+void handle_specifier(const char *format, va_list args, int *count, int *i)
+{
+	if (format[*i] == 'c')
+		handle_char(args, count);
+
+}

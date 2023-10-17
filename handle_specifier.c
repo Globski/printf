e1368bc3011631b72f7440283360ae31dede3044
Globski <gloria.ogunsemore@gmail.com>
2023-10-17 06:43:14 -0700

    handle_specifier.c

---
+++ handle_specifier.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * handle_specifier - Handle format specifiers in the printf function.
+ * @format: The format string being processed.
+ * @args: The va_list containing the arguments.
+ * @count: A pointer to the character count.
+ * @i: A pointer to the current position in the format string.
+ */

17b6e99a12b9690abb7e4e48b48fea043faa89b4
Manuelshub <kcemmy03@gmail.com>
2023-10-17 07:54:53 -0700

    Function that checks for integers

---
+++ handle_int.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * handle_int - Handles the '%d' or '%i' specifier in printf function.
+ * @args: va_list containing arguments
+ * @count: pointer to the number of characters.
+ */
+void handle_int(va_list args, int *count)
+{
+	int num = va_arg(args, int);
+	int div = 1;
+
+	if (num < 0)
+	{
+		_putchar('-');
+		(*count)++;
+		num = -num;
+	}
+	while ((num / div) > 9)
+	{
+		div *= 10;
+	}
+	while (div >= 1)
+	{
+		_putchar(((num / div) % 10) + '0');
+		(*count)++;
+		div /= 10;
+	}
+}

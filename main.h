b15c252763673c978a410bfe09b4aad2262c41ee
Gloriana <141042662+Globski@users.noreply.github.com>
2023-10-17 15:51:29 -0700

    Add prototype to main.h

--- main.h
+++ main.h
@@ -18,7 +18,7 @@ void handle_string(va_list args, int *count);
 void handle_char(va_list args, int *count);
 void handle_percent(int *count);
 int _printf(const char *format, ...);
-void handle_specifier(const char *format, va_list args, int *count, int *i);
+void handle_specifier(const char *format, va_list args __attribute__((unused)), int *count, int *i);
 void handle_int(va_list args, int *count);
 void handle_uint(va_list args, int *count);
 void print_binary(unsigned int num, int *count);

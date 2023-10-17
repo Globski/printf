3fceec6c8f80b91291072f7edf02441e6462ee75
Gloriana <141042662+Globski@users.noreply.github.com>
2023-10-17 07:08:56 -0700

    Remove trailing whitespace.

--- _printf.c
+++ _printf.c
@@ -1,12 +1,11 @@
 #include "main.h"
+
 /**
  * _printf - Custom printf function that handles %c, %s, and %%.
  * @format: The format string to be printed.
  *
  * Return: The number of characters printed.
  */
-
-
 int _printf(const char *format, ...)
 {
 	va_list args;

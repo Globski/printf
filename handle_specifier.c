bd47e73bc5813d4d39193afad3e07d8e9073af25
Manuelshub <kcemmy03@gmail.com>
2023-10-17 12:42:40 -0700

    implemented 465b6746b6a4e2b45761the format specifier for unsigned integer

--- handle_specifier.c
+++ handle_specifier.c
@@ -12,17 +12,15 @@ void handle_specifier(const char *format, va_list args, int *count, int *i)
 	if (format[*i] == 'c')
 		handle_char(args, count);
 	else if (format[*i] == 's')
-	{
 		handle_string(args, count);
-	}
 	else if (format[*i] == '%')
 		handle_percent(count);
 	else if (format[*i] == 'd' || format[*i] == 'i')
 		handle_int(args, count);
 	else if (format[*i] == 'b')
-	{
 		handle_binary(args, count);
-	}
+	else if (format[*i] == 'u')
+		handle_uint(args, count);
 	else
 	{
 		_putchar('%');

3ff81f12eaa797ff7393ef68957e6ea3f989c483
Globski <gloria.ogunsemore@gmail.com>
2023-10-18 15:42:59 -0700

    Implemented the function conversions-handler.c

---
+++ conversions-handler.c
@@ -0,0 +1,147 @@
+#include "main.h"
+
+/**
+ * handle_short_int - Handles and prints a short integer argument
+ * @args: A va_list of arguments
+ * @count: A pointer to the character count
+ *
+ * Description: This function extracts a short integer from va_list
+ * args, converts it to a string, and prints it. If the integer is
+ * negative, it adds a '-' sign to the output.
+ */
+void handle_short_int(va_list args, int *count)
+{
+	short num = va_arg(args, int);
+	int div = 1;
+
+	if (num < 0)
+	{
+		_putchar('-');
+		(*count)++;
+		num = -num;
+	}
+
+	while (num / div > 9)
+	{
+		div *= 10;
+	}
+
+	while (div >= 1)
+	{
+		_putchar((num / div) % 10 + 48);
+		(*count)++;
+		div /= 10;
+	}
+}
+
+/**
+ * handle_unsigned - Handles and prints an unsigned integer argument
+ * @args: A va_list of arguments
+ * @count: A pointer to the character count
+ *
+ * Description: This function extracts an unsigned integer from
+ * va_list args, converts it to a string, and prints it.
+ */
+void handle_unsigned(va_list args, int *count)
+{
+	unsigned int num = va_arg(args, unsigned int);
+
+	unsigned int div = 1;
+
+	while (num / div > 9)
+	{
+		div *= 10;
+	}
+
+	while (div >= 1)
+	{
+		_putchar((num / div) % 10 + '0');
+		(*count)++;
+		div /= 10;
+	}
+}
+
+/**
+ * handle_short_unsigned - Handles and prints a short unsigned integer argument
+ * @args: A va_list of arguments
+ * @count: A pointer to the character count
+ *
+ * Description: This function extracts a short unsigned integer from
+ * va_list args, converts it to a string, and prints it.
+ */
+void handle_short_unsigned(va_list args, int *count)
+{
+	unsigned short num = va_arg(args, int);
+	int div = 1;
+
+	while (num / div > 9)
+	{
+		div *= 10;
+	}
+
+	while (div >= 1)
+	{
+		_putchar((num / div) % 10 + 48);
+		(*count)++;
+		div /= 10;
+	}
+}
+
+/**
+ * handle_long_unsigned - Handles and prints a long unsigned integer argument
+ * @args: A va_list of arguments
+ * @count: A pointer to the character count
+ *
+ * Description: This function extracts a long unsigned integer from
+ * va_list args, converts it to a string, and prints it.
+ */
+void handle_long_unsigned(va_list args, int *count)
+{
+	unsigned long num = va_arg(args, unsigned long);
+	unsigned long div = 1;
+
+	while (num / div > 9)
+	{
+		div *= 10;
+	}
+
+	while (div >= 1)
+	{
+		_putchar((num / div) % 10 + 48);
+		(*count)++;
+		div /= 10;
+	}
+}
+
+/**
+ * handle_long_int - Handles and prints a long integer argument
+ * @args: A va_list of arguments
+ * @count: A pointer to the character count
+ *
+ * Description: This function extracts a long integer from va_list args,
+ * converts it to a string, and prints it. It also handles negative values.
+ */
+void handle_long_int(va_list args, int *count)
+{
+	long num = va_arg(args, long);
+	int div = 1;
+
+	if (num < 0)
+	{
+		_putchar('-');
+		(*count)++;
+		num = -num;
+	}
+
+	while (num / div > 9)
+	{
+		div *= 10;
+	}
+
+	while (div >= 1)
+	{
+		_putchar((num / div) % 10 + 48);
+		(*count)++;
+		div /= 10;
+	}
+}

30602e94fbfb78f1a0570266143f89823c6f538f
Gloriana <141042662+Globski@users.noreply.github.com>
2023-10-17 19:14:50 -0700

    Update handle_specifier.c

--- handle_specifier.c
+++ handle_specifier.c
@@ -28,9 +28,9 @@ void handle_specifier(const char *format, va_list args, int *count, int *i)
 	else if (format[*i] == 'X')
 		handle_hex(args, count, 1);
 	else if (format[*i] == 'p')
-	{
 		handle_pointer(args, count);
-    	}
+	else if (format[*i] == 'S')
+        	handle_string_custom(args, count);
 	else
 	{
 		_putchar('%');

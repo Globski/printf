3dd8c968dd519eba3b95bd4d61c028e381eaeb84
Globski <gloria.ogunsemore@gmail.com>
2023-10-17 18:05:15 -0700

    Implement the function handle_pointer.c

---
+++ handle_pointer.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * handle_pointer - Format and print a pointer.
+ * @args: A va_list containing the pointer to be printed.
+ * @count: A pointer to the count of characters printed.
+ */
+void handle_pointer(va_list args, int *count)
+{
+	void *ptr = va_arg(args, void *);
+
+	_putchar('0');
+	_putchar('x');
+	(*count) += 2;
+
+	if (ptr != NULL)
+	{
+		unsigned long num = (unsigned long)ptr;
+		char hex_nums[] = "0123456789abcdef";
+
+		int flagStart = 0;
+
+		for (int i = (sizeof(void *) * 2) - 1; i >= 0; i--)
+		{
+			int digit = (num >> (i * 4)) & 0xf;
+
+			if (digit != 0 || flagStart)
+			{
+				_putchar(hex_nums[digit]);
+				(*count)++;
+				flagStart = 1;
+			}
+		}
+	}
+	else
+	{
+		char nil[] = "(nil)";
+
+		for (int i = 0; nil[i]; i++)
+		{
+			_putchar(nil[i]);
+			(*count)++;
+		}
+	}
+}

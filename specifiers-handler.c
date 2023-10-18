df0ed8a9b6cfde76ea39550ce220ad22e81276a1
Globski <gloria.ogunsemore@gmail.com>
2023-10-18 16:06:10 -0700

    specifiers-handler.c

--- specifiers-handler.c
+++ specifiers-handler.c
@@ -17,27 +17,27 @@ void handle_specifier(const char *format, va_list args, int *count, int *i)
 		handle_percent(count);
 	else if (format[*i] == 'd' || format[*i] == 'i')
 	{
-        if (format[*i + 1] == 'l')
-        {
-		handle_long_int(args, count);
-		(*i)++;
-	}
-	else
-	{
-		handle_integer(args, count);
-	}
+		if (format[*i + 1] == 'l')
+		{
+			handle_long_int(args, count);
+			(*i)++;
+		}
+		else
+		{
+			handle_integer(args, count);
+		}
 	}
 	else if (format[*i] == 'u')
 	{
-        if (format[*i + 1] == 'l' && format[*i + 2] == 'l')
-        {
-            handle_long_unsigned(args, count);
-            (*i) += 2;
-        }
-        else
-	{
-		handle_unsigned(args, count);
-    	}
+		if (format[*i + 1] == 'l' && format[*i + 2] == 'l')
+		{
+			handle_long_unsigned(args, count);
+			(*i) += 2;
+		}
+		else
+		{
+			handle_unsigned(args, count);
+		}
 	}
 	else if (format[*i] == 'b')
 		handle_binary(args, count);

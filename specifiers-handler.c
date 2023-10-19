1fe13bc437aa628e0a807918e9079cea6338cc29
Gloriana <141042662+Globski@users.noreply.github.com>
2023-10-18 17:49:51 -0700

    Update specifiers-handler.c

--- specifiers-handler.c
+++ specifiers-handler.c
@@ -15,8 +15,8 @@ void handle_specifier(const char *format, va_list args, int *count, int *i)
 		handle_string(args, count);
 	else if (format[*i] == '%')
 		handle_percent(count);
-	else if (format[*i] == 'd' || format[*i] == 'i')
-		handle_int_or_long(args, count, format, i);
+	else if (format[*i] == 'd' || format[*i] == 'i' || format[*i] == 'u')
+		handle_length_modifier(format, args, count, i);
 	else if (format[*i] == 'u')
 		handle_unsigned(args, count, format, i);
 	else if (format[*i] == 'b')
@@ -40,39 +40,46 @@ void handle_specifier(const char *format, va_list args, int *count, int *i)
 }
 
 /**
- * handle_unsigned - Handles the 'u' format specifier, which can be
- * 'u' or 'ull'.
+ * handle_length_modifier - Handles length modifier for numeric specifiers.
+ * @format: The format string being processed.
  * @args: The va_list containing the arguments.
  * @count: A pointer to the character count.
- * @format: The format string being processed.
  * @i: A pointer to the current position in the format string.
+ *
+ * Description: This function processes the length modifier for
+ * numeric specifiers, such as 'd' and 'u'. It determines whether to
+ * use long or short variants of the specifiers based on the format string.
  */
-void handle_unsigned(va_list args, int *count, const char *format, int *i)
+void handle_length_modifier(const char *format, va_list args, int *count, int *i)
 {
-	if (format[*i + 1] == 'l' && format[*i + 2] == 'l')
+	if (format[*i] == 'd' || format[*i] == 'i')
 	{
-		handle_long_unsigned(args, count);
-		(*i) += 2;
+		if (format[*i + 1] == 'l')
+		{
+			print_long_int(args, count);
+			(*i)++;
+		}
+		else if (format[*i + 1] == 'h')
+		{
+			print_short_int(args, count);
+			(*i)++;
+		}
+		else
+			handle_int(args, count);
 	}
-	else
-		handle_unsigned_int(args, count);
-}
-
-/**
- * handle_int_or_long - Handles the 'd' and 'i' format specifiers, which
- * can be 'd', 'i', or 'll'.
- * @args: The va_list containing the arguments.
- * @count: A pointer to the character count.
- * @format: The format string being processed.
- * @i: A pointer to the current position in the format string.
- */
-void handle_int_or_long(va_list args, int *count, const char *format, int *i)
-{
-	if (format[*i + 1] == 'l')
+	else if (format[*i] == 'u')
 	{
-		handle_long_int(args, count);
-		(*i)++;
+		if (format[*i + 1] == 'l')
+		{
+			print_long_unsigned_int(args, count);
+			(*i)++;
+		}
+		else if (format[*i + 1] == 'h')
+		{
+			print_short_unsigned_int(args, count);
+			(*i)++;
+		}
+		else
+			handle_uint(args, count);
 	}
-	else
-		handle_integer(args, count);
 }

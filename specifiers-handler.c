33a9dd3b4e63bb17e3d93e76c1643c73805cafc9
Gloriana <141042662+Globski@users.noreply.github.com>
2023-10-18 17:03:08 -0700

    Update specifiers-handler.c

--- specifiers-handler.c
+++ specifiers-handler.c
@@ -16,29 +16,11 @@ void handle_specifier(const char *format, va_list args, int *count, int *i)
 	else if (format[*i] == '%')
 		handle_percent(count);
 	else if (format[*i] == 'd' || format[*i] == 'i')
-	{
-		if (format[*i + 1] == 'l')
-		{
-			handle_long_int(args, count);
-			(*i)++;
-		}
-		else
-			handle_integer(args, count);
-	}
+		handle_integer_or_long(args, count, format, i);
 	else if (format[*i] == 'u')
-	{
-		if (format[*i + 1] == 'l' && format[*i + 2] == 'l')
-		{
-			handle_long_unsigned_int(args, count);
-			(*i) += 2;
-		}
-		else
-			handle_unsigned_int(args, count);
-	}
+		handle_unsigned(args, count, format, i);
 	else if (format[*i] == 'b')
 		handle_binary(args, count);
-	else if (format[*i] == 'u')
-		handle_uint(args, count);
 	else if (format[*i] == 'o')
 		handle_octal(args, count);
 	else if (format[*i] == 'x')
@@ -55,4 +37,41 @@ void handle_specifier(const char *format, va_list args, int *count, int *i)
 		_putchar(format[*i]);
 		(*count) += 2;
 	}
+}
+
+/**
+ * handle_unsigned - Handles the 'u' format specifier, which can be
+ * 'u' or 'ull'.
+ * @args: The va_list containing the arguments.
+ * @count: A pointer to the character count.
+ * @format: The format string being processed.
+ * @i: A pointer to the current position in the format string.
+ */
+void handle_unsigned(va_list args, int *count, const char *format, int *i)
+{
+	if (format[*i + 1] == 'l' && format[*i + 2] == 'l')
+	{
+		handle_long_unsigned(args, count);
+		(*i) += 2;
+	}
+	else
+		handle_unsigned_int(args, count);
+}
+
+/**
+ * handle_integer_or_long - Handles the 'd' and 'i' format specifiers, which can be 'd', 'i', or 'll'.
+ * @args: The va_list containing the arguments.
+ * @count: A pointer to the character count.
+ * @format: The format string being processed.
+ * @i: A pointer to the current position in the format string.
+ */
+void handle_integer_or_long(va_list args, int *count, const char *format, int *i)
+{
+	if (format[*i + 1] == 'l')
+	{
+		handle_long_int(args, count);
+		(*i)++;
+	}
+	else
+		handle_integer(args, count);
 }

36d406a93f7c8e0ede7b5076506c00866fddf1d6
Gloriana <141042662+Globski@users.noreply.github.com>
2023-10-18 19:31:30 -0700

    Update specifiers-handler.c

--- specifiers-handler.c
+++ specifiers-handler.c
@@ -43,85 +43,41 @@ void handle_specifier(const char *format, va_list args, int *count, int *i)
  * @args: The va_list containing the arguments.
  * @count: A pointer to the character count.
  * @i: A pointer to the current position in the format string.
+ *
+ * Description: This function processes the length modifier for
+ * numeric specifiers, such as 'd' and 'u'. It determines whether to
+ * use long or short variants of the specifiers based on the format string.
  */
-void handle_len_modifier(const char *format, va_list args, int *count, int *i)
+void handle_length_modifier(const char *format, va_list args, int *count, int *i)
 {
-    if (format[*i] == 'd' || format[*i] == 'i')
-    {
-        if (format[*i + 1] == 'l')
-        {
-            print_long_int(args, count);
-            (*i)++;
-        }
-        else if (format[*i + 1] == 'h')
-        {
-            print_short_int(args, count);
-            (*i)++;
-        }
-        else
-            handle_int(args, count);
-    }
-    else if (format[*i] == 'u')
-    {
-        if (format[*i + 1] == 'l')
-        {
-            print_long_unsigned_int(args, count);
-            (*i)++;
-        }
-        else if (format[*i + 1] == 'h')
-        {
-            print_short_unsigned_int(args, count);
-            (*i)++;
-        }
-        else
-            handle_uint(args, count);
-    }
-    else if (format[*i] == 'o')
-    {
-        if (format[*i + 1] == 'l')
-        {
-            handle_octal(args, va_arg(args, unsigned long));
-            (*i)++;
-        }
-        else if (format[*i + 1] == 'h')
-        {
-            handle_uint(args, va_arg(args, unsigned int));
-            (*i)++;
-        }
-        else
-        {
-            handle_uint(args, count);
-        }
-    }
-    else if (format[*i] == 'x')
-    {
-        if (format[*i + 1] == 'l')
-        {
-            print_long_unsigned_int(args, va_arg(args, unsigned long));
-            (*i)++;
-        }
-        else if (format[*i + 1] == 'h')
-        {
-            handle_uint(args, va_arg(args, unsigned int));
-            (*i)++;
-        }
-        else
-            handle_uint(args, count);
-    }
-    else if (format[*i] == 'X')
-    {
-        if (format[*i + 1] == 'l')
-        {
-            print_long_unsigned_int(args, va_arg(args, unsigned long));
-            (*i)++;
-        }
-        else if (format[*i + 1] == 'h')
-        {
-            print_short_unsigned_int(args, va_arg(args, unsigned int));
-            (*i)++;
-        }
-        else
-            handle_uint(args, count);
-    }
+	if (format[*i] == 'd' || format[*i] == 'i')
+	{
+		if (format[*i + 1] == 'l')
+		{
+			print_long_int(args, count);
+			(*i)++;
+		}
+		else if (format[*i + 1] == 'h')
+		{
+			print_short_int(args, count);
+			(*i)++;
+		}
+		else
+			handle_int(args, count);
 	}
-
+	else if (format[*i] == 'u')
+	{
+		if (format[*i + 1] == 'l')
+		{
+			print_long_unsigned_int(args, count);
+			(*i)++;
+		}
+		else if (format[*i + 1] == 'h')
+		{
+			print_short_unsigned_int(args, count);
+			(*i)++;
+		}
+		else
+			handle_uint(args, count);
+	}
+}
